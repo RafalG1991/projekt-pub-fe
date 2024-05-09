ecf92ea0e223b41e83d299dc25e8637f52ecad23
Rafal <rgrajcar@gmail.com>
2024-05-09 22:35:12 +0200

    added table view styling

--- src/components/Tables.js
+++ src/components/Tables.js
@@ -62,19 +62,31 @@ const Tables = () => {
   return (
     <div className="restaurant-tables-container">
       {tablesData.tables?.map((table) => (
-        <div key={table[0]} className={`table ${table[3]}`} onClick={() => openOrder(table[3], table[0], 1)}>
-          <p>Table {table[1]}</p>
-          <p>Table capacity {table[2]}</p>
-          <p>Status: {table[3]}</p>
-          {table[3] === 'BUSY' ? <button type="button" onClick={() => {
-            showOrder(table[3], table[0])
-            toggleOrder()
-          }
-          }>Show order</button> : ''}
-          {table[3] === 'BUSY' ? <button type="button" onClick={() => closeOrder(table[3], table[0])}>Close order</button> : ''}
-        </div>
-      ))}
-      {orderToggle && <OrderModal order={order} toggleOrder={toggleOrder}/>}
+          <div key={table[0]} className={`table-card ${table[3]}`} onClick={() => openOrder(table[3], table[0], 1)}>
+            <div className="table-details">
+              <h2 className="table-number">Table {table[1]}</h2>
+              <p className="table-capacity">Capacity: {table[2]} people</p>
+              <p className="table-status">{table[3]}</p>
+              {table[3] === 'FREE' && (
+                <div className="reserve-message">Click to reserve</div>
+              )}
+            </div>
+            <div className="table-buttons">
+              {table[3] === 'BUSY' ? <button type="button" className="table-button show-order-button" onClick={() => {
+                showOrder(table[3], table[0])
+                toggleOrder()
+              }
+              }>Show order</button> : ''}
+              {table[3] === 'BUSY' ?
+                <button type="button" className="table-button close-order-button"
+                        onClick={() => closeOrder(table[3], table[0])}>Close order</button> : ''}
+            </div>
+          </div>
+        )
+      )}
+      {
+        orderToggle && <OrderModal order={order} toggleOrder={toggleOrder}/>
+      }
     </div>
   );
 };
--- src/components/TablesView.js
+++ src/components/TablesView.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Tables from "./Tables";
 
-import './TablesView.css';
 import Navigation from "./Navigation";
 const TablesView = () => {
 
   return (
     <>
       <Navigation/>
-      <div className="table-wrapper">
-        <div>
-          <h1>Pub Tables</h1>
-          <Tables/>
-        </div>
-      </div>
+      <Tables/>
     </>
   );
 }
