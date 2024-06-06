cf7e769745c47b6a9c84d5aa59651eb81f9982a1
Rafal <rgrajcar@gmail.com>
2024-06-06 18:52:23 +0200

    added ingredients menu

---
+++ src/components/Supplies.js
@@ -0,0 +1,44 @@
+import './Menu.css';
+import Navigation from "./Navigation";
+import React, {useEffect, useState} from "react";
+import {Loader} from "./Loader";
+import SupplyItem from "./SupplyItem";
+
+import "react-toastify/dist/ReactToastify.css";
+import {toast, ToastContainer} from "react-toastify";
+
+const Supplies = props => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [suppliesData, setSuppliesData] = useState([]);
+    const [reload, setReload] = useState(false);
+
+    useEffect(() => {
+        setIsLoading(true);
+        // fetch('https://projekt-pub.onrender.com/report/inventory')
+        fetch('http://localhost:5000/report/inventory')
+          .then(response => response.json())
+          .then(r => {
+              setIsLoading(false);
+              setSuppliesData(r);
+          });
+        if(reload) {
+          toast.success("Ingredient successfully added!", {
+            position: "bottom-center"
+          });
+          setReload(false);
+        }
+    }, [reload]);
+
+    return (
+      <>
+          <Navigation/>
+          {isLoading ? <Loader /> : <div className="menu-wrapper">
+            {suppliesData.inv?.map(item => <SupplyItem key={item[0]} item={item} setReload={setReload}/>)}
+          </div>}
+          <ToastContainer/>
+      </>
+
+    );
+}
+
+export default Supplies;
